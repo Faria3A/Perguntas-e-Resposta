f39be832ccb84e4017c905b4fa35c09de9f14f38
Faria3A <veigakawan7@gmail.com>
2024-11-07 20:57:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sobre a Disney',
+    'Qual princesa long-metragem de animação lançado pela Disney?',
+    'Branca de Neve e os Sete Anões (1937)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em termos de área territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Qual é a classe gramatical das palavras que indicam ação, estado ou fenômeno da natureza?',
+    'Verbo'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado da soma dos ângulos internos de um triângulo?',
+    '180 graus'
+)
